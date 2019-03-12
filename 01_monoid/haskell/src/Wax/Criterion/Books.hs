7beac8bc196347d274743fcca34631f77db47157
Borys Bulyha <borysb@wix.com>
2019-03-12 15:51:01 +0200

    [monoid][hs] improve books path handling

--- 01_monoid/haskell/src/Wax/Criterion/Books.hs
+++ 01_monoid/haskell/src/Wax/Criterion/Books.hs
@@ -8,20 +8,22 @@ import Wax.Books
 --------------------------------------------------------------------------------
 import Criterion.Main
 import Path
+import Path.IO
 import Prelude
 import Data.HashMap.Lazy
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = go
-  -- = defaultMain
-  -- [ bgroup "readBooks" [ bench "default" $ nfIO go ]
-  -- ]
+main
+  = defaultMain
+  [ bgroup "readBooks" [ bench "default" $ nfIO go ]
+  ]
 
 --------------------------------------------------------------------------------
 go :: IO ()
 go = do
-  books <- readBooks fp
+  fp <- getCurrentDir
+  books <- readBooks $ parent fp </> [reldir|scala/src/main/resources/books/|]
   let (WordsMap wordsMap)  = foldMap toWordsMap books
   let leastUsed = findBy (curry $ (>5) . length . fst) (<) maxBound wordsMap
   let mostUsed  = findBy (curry $ (>5) . length . fst) (>) minBound wordsMap
@@ -32,8 +34,3 @@ go = do
 findBy :: (String -> v -> Bool) -> (v -> v -> Bool) -> v -> HashMap String v -> (String, v)
 findBy f s e = foldrWithKey g ("", e)
   where g k v r@(_, vr) = if f k v && s v vr then (k, v) else r
-
---------------------------------------------------------------------------------
--- JUST FOR TESTING PURPOSES
-fp :: Path Abs Dir
-fp = [absdir|/Users/d12frosted/Developer/wax/monoid/scala/src/main/resources/books|]
