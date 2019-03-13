2d0b0b319ecd1ced2dc2a052d0179a59286b424f
valentynv <valentynv@wix.com>
2019-03-13 09:42:06 +0200

    Clean up Validated

--- functor_applicative/scala/src/main/scala/wax/validated/Validated.scala
+++ functor_applicative/scala/src/main/scala/wax/validated/Validated.scala
@@ -3,25 +3,21 @@ package wax.validated
 import cats.Applicative
 import cats.data.NonEmptyList
 import cats.kernel.Semigroup
+import scala.language.postfixOps
 import cats.implicits._
 
-import scala.language.postfixOps
+sealed trait Validated[+E, +A]
+case class Valid[+A](a: A) extends Validated[Nothing, A]
+case class Invalid[+E](e: E) extends Validated[E, Nothing]
 
 object Validated extends App {
   implicit def validatedApplicative[E: Semigroup]: Applicative[Validated[E, ?]] = ???
 
-  import Validator._
-
-  def validateConfig(config: Config): Validated[NonEmptyList[ConfigError], Config] =
-    (validatePort("appPort", config.appPort),
-      validateHost("dbHost", config.dbHost),
-      validatePort("dbPort", config.dbPort)
-    )
-      .mapN[Config]((a: Int, b: String, c: Int) => config)
+  def validateConfig(config: Config): Validated[NonEmptyList[ConfigError], Config] = ???
 
   validateConfig(Configs.validConfig) == Valid(Configs.validConfig)
   validateConfig(Configs.typicalBorisConfig) == Invalid(NonEmptyList.of(
-    ConfigError("appPort", "port must be a positive value between 0 and 65536"),
+    ConfigError("appPort", "port must be an int between 0 and 65536"),
     ConfigError("dbHost", "host must be a proper hostname/ip without port")
   ))
 }
@@ -29,15 +25,19 @@ object Validated extends App {
 object Validator {
   private val hostnameRegex = "[a-z0-9]+[\\.]{1}[a-z0-9]+[\\.]{1}[a-z0-9]+[\\.]{1}[a-z0-9]+".r
 
-  def validatePort(field: String, p: Int) =
+  def validatePort(field: String, p: Int): Validated[NonEmptyList[ConfigError], Int] =
     if (p >= 0 && p <= 65536) Valid(p)
-    else Invalid(NonEmptyList.one(ConfigError(field, "port must be a positive value between 0 and 65536")))
+    else Invalid(NonEmptyList.one(ConfigError(field, "port must be an int between 0 and 65536")))
 
-  def validateHost(field: String, h: String)= hostnameRegex.findFirstIn(h) match {
-    case Some(s) => Valid(s)
-    case None => Invalid(NonEmptyList.one(ConfigError(field, "host must be a proper hostname/ip without port")))
-  }
+  def validateHost(field: String, h: String): Validated[NonEmptyList[ConfigError], String] =
+    hostnameRegex.findFirstIn(h) match {
+      case Some(s) => Valid(s)
+      case None => Invalid(NonEmptyList.one(ConfigError(field, "host must be a proper hostname/ip without port")))
+    }
 }
+
+case class Config(appPort: Int, dbHost: String, dbUsername: String, dbPw: String, dbSchema: String, dbPort: Int)
+case class ConfigError(field: String, message: String)
 
 object Configs {
   val validConfig = Config(
@@ -58,10 +58,3 @@ object Configs {
     dbPort     = 3300,
   )
 }
-
-case class Config(appPort: Int, dbHost: String, dbUsername: String, dbPw: String, dbSchema: String, dbPort: Int)
-case class ConfigError(field: String, message: String)
-
-sealed trait Validated[+E, +A]
-case class Valid[+A](a: A) extends Validated[Nothing, A]
-case class Invalid[+E](e: E) extends Validated[E, Nothing]
