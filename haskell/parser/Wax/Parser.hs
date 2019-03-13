bc1cd078182ceea104df1e0bbb94040fc54365a0
Borys Bulyha <borysb@wix.com>
2019-03-13 19:39:27 +0200

    actually simplify the parser

--- haskell/parser/Wax/Parser.hs
+++ haskell/parser/Wax/Parser.hs
@@ -5,12 +5,12 @@ module Wax.Parser where
 import Prelude
 import Control.Applicative
 
-data Result a = Success String a | Failure String String
+data Result a = Success String a | Failure
 
 newtype Parser a = Parser { parse :: String -> Result a }
 
 instance Functor Result where
-  fmap _ (Failure s e) = Failure s e
+  fmap _ Failure       = Failure
   fmap f (Success s a) = Success s $ f a
 
 instance Functor Parser where
@@ -19,25 +19,28 @@ instance Functor Parser where
 instance Applicative Parser where
   pure a = Parser $ \s -> Success s a
   (Parser cs1) <*> (Parser cs2) = Parser $ \s -> case cs1 s of
-    Failure s1 e -> Failure s1 e
+    Failure      -> Failure
     Success s1 f -> f <$> cs2 s1
 
 instance Alternative Parser where
-  empty   = Parser $ \_ -> Failure "" "Empty parser"
+  empty   = Parser $ \_ -> Failure
   p <|> q = Parser $ \s -> case parse p s of
-    Failure _ _     -> parse q s
+    Failure         -> parse q s
     r@(Success _ _) -> r
 
-satisfy :: (Char -> Bool) -> String -> Parser Char
-satisfy p e = Parser parser
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser parser
   where parser (x:xs) | p x = Success xs x
-        parser s            = Failure s e
+        parser _            = Failure
 
 anyChar :: Parser Char
-anyChar = satisfy (\_ -> True) ""
+anyChar = satisfy (\_ -> True)
 
 char :: Char -> Parser Char
-char c = satisfy (c ==) (show c)
+char = satisfy . (==)
+
+notChar :: Char -> Parser Char
+notChar = satisfy . (/=)
 
 space :: Parser Char
 space = char ' '
@@ -54,19 +57,8 @@ runParser :: Parser a -> String -> Either String a
 runParser m s = case parse m s of
   Success "" a -> Right a
   Success s0 _ -> Left $ "parser did not consume entire stream: " <> s0
-  Failure s0 e -> Left $ concat
-    [ "parser error", "\n"
-    , "  expected: ", e, "\n"
-    , "     input: \"", s0, "\""
-    ]
+  Failure      -> Left $ "parser error"
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main =
-  let t = "hello"
-      i = "    hells  "
-  in do
-    putStrLn $ "Going to parse token '" <> t <> "' in '" <> i <> "'"
-    case runParser (token . string $ t) i of
-      Left e -> putStrLn e
-      Right _ -> putStrLn "success"
+main = putStrLn "nothing to do"
