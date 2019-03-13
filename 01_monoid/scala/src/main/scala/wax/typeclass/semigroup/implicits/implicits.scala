f5d20c8f54f4805b2c5a670474b90bae7b1f30d2
Borys Bulyha <borysb@wix.com>
2019-03-13 14:12:59 +0200

    [monoid] remove impl of semigroup instances

--- 01_monoid/scala/src/main/scala/wax/typeclass/semigroup/implicits/implicits.scala
+++ 01_monoid/scala/src/main/scala/wax/typeclass/semigroup/implicits/implicits.scala
@@ -2,11 +2,11 @@ package wax.typeclass.semigroup
 
 package object implicits {
   implicit val stringSemigroup: Semigroup[String] = new Semigroup[String] {
-    override def append(x: String, y: String): String = x + y
+    override def append(x: String, y: String): String = ???
   }
 
   implicit def listSemigroup[A]: Semigroup[List[A]] = new Semigroup[List[A]] {
-    override def append(x: List[A], y: List[A]): List[A] = x ++ y
+    override def append(x: List[A], y: List[A]): List[A] = ???
   }
 
   implicit class SemigroupOps[A: Semigroup](x: A) {
