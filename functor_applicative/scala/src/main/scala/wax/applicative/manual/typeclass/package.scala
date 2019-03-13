8bfef0ba35d345ae2eb74bed2818012dc3f7ff8f
valentynv <valentynv@wix.com>
2019-03-13 13:49:22 +0200

    Add Functor and Applicative laws

--- functor_applicative/scala/src/main/scala/wax/applicative/manual/typeclass/package.scala
+++ functor_applicative/scala/src/main/scala/wax/applicative/manual/typeclass/package.scala
@@ -6,7 +6,23 @@ package object typeclass {
 
   //
   // Laws:
-  // TODO
+  // 1. Identity:
+  //    (fa: F[A]) =>
+  //      F.pure((a: A) => a).ap(fa) <-> fa
+  // 2. Homomorphism:
+  //    (a: A, f: A => B) =>
+  //      F.pure(f).ap(F.pure(a)) <-> F.pure(f(a))
+  // 3. Interchange:
+  //    (a: A, ff: F[A => B]) =>
+  //      ff.ap(F.pure(a)) <-> F.pure((f: A => B) => f(a)).ap(ff)
+  // 4. Map:
+  //    (fa: F[A], f: A => B) =>
+  //      fa.map(f) <-> F.pure(f).ap(fa)
+  // 5. Composition:
+  //    (fa: F[A], fab: F[A => B], fbc: F[B => C]) => {
+  //      val compose: (B => C) => (A => B) => (A => C) = _.compose
+  //      F.pure(compose).ap(fbc).ap(fab).ap(fa) <-> fbc.ap(fab.ap(fa))
+  //    }
   //
   trait Applicative[F[_]] {
     def ap[A, B](f: F[A => B])(fa: F[A]): F[B]
--- functor_applicative/scala/src/main/scala/wax/functor/manual/typeclass/package.scala
+++ functor_applicative/scala/src/main/scala/wax/functor/manual/typeclass/package.scala
@@ -6,7 +6,12 @@ package object typeclass {
 
   //
   // Laws:
-  // TODO
+  // 1. Identity:
+  //    (fa: F[A]) =>
+  //      fa.map(identity) <-> fa
+  // 2. Composition:
+  //    (fa: F[A], f: A => B, g: B => C) =>
+  //      fa.map(f).map(g) <-> fa.map(f andThen g)
   //
   trait Functor[F[_]] {
     def fmap[A, B](f: A => B)(fa: F[A]): F[B]
