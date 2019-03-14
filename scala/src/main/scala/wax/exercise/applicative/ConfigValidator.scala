5c4ec5da68358772c00cc4d1bf0bb472a0528b3e
valentynv <valentynv@wix.com>
2019-03-14 11:13:34 +0200

    More cleanup

--- scala/src/main/scala/wax/exercise/applicative/ConfigValidator.scala
+++ scala/src/main/scala/wax/exercise/applicative/ConfigValidator.scala
@@ -13,20 +13,17 @@ case class Invalid[+E](e: E) extends Validated[E, Nothing]
 case class ConfigValidationError(field: String, message: String)
 
 object ConfigValidator {
-  private val hostnameRegex = "[a-z0-9]+[\\.]{1}[a-z0-9]+[\\.]{1}[a-z0-9]+[\\.]{1}[a-z0-9]+".r
 
   private def validatePort(field: String, p: Int): Validated[NonEmptyList[ConfigValidationError], Int] =
     if (p >= 0 && p <= 65536) Valid(p)
     else Invalid(NonEmptyList.one(ConfigValidationError(field, "port must be an int between 0 and 65536")))
 
   private def validateHost(field: String, h: String): Validated[NonEmptyList[ConfigValidationError], String] =
-    hostnameRegex.findFirstIn(h) match {
-      case Some(s) => Valid(s)
-      case None => Invalid(NonEmptyList.one(ConfigValidationError(field, "host must be a proper hostname/ip without port")))
-    }
+    if (h.matches("[a-z0-9]+[\\.]{1}[a-z0-9]+[\\.]{1}[a-z0-9]+[\\.]{1}[a-z0-9]+")) Valid(h)
+    else Invalid(NonEmptyList.one(ConfigValidationError(field, "host must be a proper hostname/ip without port")))
 
 
-  implicit def validatedApplicative[E: Semigroup]: Applicative[Validated[E, ?]] = ???
+  implicit val validatedApplicative: Applicative[Validated[NonEmptyList[ConfigValidationError], Config]] = ???
 
   //TODO Validate config using applicative
   def validateConfig(config: Config): Validated[NonEmptyList[ConfigValidationError], Config] = ???
