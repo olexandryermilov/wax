bb41a1239e6e0e99b38ef93b926b608a3bccc471
valentynv <valentynv@wix.com>
2019-07-23 11:34:44 +0300

    remove redundant todo

--- scala/src/main/scala/wax/exercise/monadicparser/ConfigValidator.scala
+++ scala/src/main/scala/wax/exercise/monadicparser/ConfigValidator.scala
@@ -10,10 +10,9 @@ sealed trait Validated[+E, +A]
 case class Valid[+A](a: A) extends Validated[Nothing, A]
 case class Invalid[+E](e: E) extends Validated[E, Nothing]
 
-//TODO Valik: think of an application for a monad here
 object ConfigValidator {
 
-  private def validateHost(field: String, h: String): Validated[NonEmptyList[String], String] =
+  private def validateHost(h: String): Validated[NonEmptyList[String], String] =
     if (h.matches("[a-z0-9]+[\\.]{1}[a-z0-9]+[\\.]{1}[a-z0-9]+[\\.]{1}[a-z0-9]+")) Valid(h)
     else Invalid(NonEmptyList.one("host must be a proper hostname/ip without port"))
 
